573b65ada2ae178e5ca9b4998c18cbf6a02101a8
Robin Stacey Doorenbosch <doorenboschrs@gmail.com>
2018-10-17 14:38:19 +0200

    This is a test

--- Final Assignment NS-kaartautomaat.py
+++ Final Assignment NS-kaartautomaat.py
@@ -12,6 +12,35 @@ def inlezen_beginstation(stations):
 beginstation = inlezen_beginstation(stations)
 
 def inlezen_eindstation(stations, beginstation):
+    beginstation
     es = input("Type hier uw eindstation in: ")
+    if es in stations:
+        if  stations.index(es) >= stations.index(beginstation):
+            return es
+        else:
+            print("Error! Type een ander station in.")
+            inlezen_eindstation(stations, beginstation)
 
+    else:
+        print("Error! Type een ander station in.")
+        inlezen_eindstation(stations, beginstation)
+
+
+eindstation = inlezen_eindstation(stations, beginstation)
+
+
+def omroepen_reis(stations, beginstation, eindstation):
+    beginstation
+    eindstation
+    print("Het beginstation " + beginstation + " is het " + str(stations.index(beginstation)+1) + "e station in het traject.")
+    print("Het eindstation " + eindstation + " is het " + str(stations.index(eindstation)+1) + "e station in het traject.")
+    print("De afstand bedraagt " + str(stations.index(eindstation) - stations.index(beginstation)) + " stations(s).")
+    print("De prijs van het kaartje is " + str((stations.index(eindstation) - stations.index(beginstation)) * 5) + " euro." + "\n")
+    print("Jij stapt in: " + beginstation)
+    for s in stations:
+        if stations.index(s) > stations.index(beginstation) and stations.index(eindstation) > stations.index(s):
+            print("- " + s)
+    print("Jij tapt uit in: " + eindstation)
+
+omroepen_reis(stations, beginstation, eindstation)
 
--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
-cijferlijst = {"Bob":8, "Lenord":10, "Brad":2, "Jan":1, "Hans":6, "George":9, "Roos":10, "Britney":1 }
-test = cijferlijst.items()
+test = ["robin", "maura"]
 
+print(test.index("robin"))
+print(test.index("maura"))
